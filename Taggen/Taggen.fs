c688ed03cead37d61e35e1f7dac0d9e096f8dc59
Michael Newton <michael@mavnn.co.uk>
2011-12-07 10:28:44 +0000

    Initial commit of basic functionality.

---
+++ Taggen/Taggen.fs
@@ -0,0 +1,47 @@
+﻿module Taggen
+open System.Text.RegularExpressions
+open System.Collections
+
+let RegMatch pattern input =
+    let matches = Regex.Matches(input, pattern)
+    matches |> Seq.cast
+    |> Seq.map (fun (m : Match) ->
+        [for g in m.Groups -> (g.Value)])
+
+let inline foldStrings strings = Seq.fold (fun acc item -> acc + item) "" strings
+
+let writeTagname tagText = 
+    RegMatch (@"\w+") tagText
+    |> Seq.head 
+    |> List.head
+
+let rec writeClasses tagText =
+    let classes = RegMatch (@"\.\w+") tagText |> Seq.concat |> Seq.map (fun dottedName -> dottedName.Trim '.')
+    match Seq.length classes with
+    | 0 -> ""
+    | 1 -> sprintf " class=\"%s\"" (Seq.head classes)
+    | _ -> Seq.fold (fun acc item -> acc + " " + item) (Seq.head classes) (Seq.skip 1 classes) |> sprintf " class=\"%s\""
+
+let writeId tagText =
+    let matches = RegMatch (@"#\w+") tagText
+    if (Seq.length matches > 1) then failwith "An element can only have one id"
+    else
+        Seq.map (fun groups -> groups |> List.map (fun (x : string) -> sprintf " id=\"%s\"" (x.Trim('#'))) |> foldStrings) matches |> foldStrings
+
+type Fragment =
+    | Text of string
+    | Frag of string * (Fragment seq)
+
+let rec printFrag fragment =
+    match fragment with
+    | Text t -> t
+    | Frag (tag, children) ->
+        let tagName = writeTagname tag
+        let classes = writeClasses tag
+        let id = writeId tag
+        let openContents = sprintf "%s%s%s" tagName classes id
+        let innerText = Seq.map printFrag children |> foldStrings
+        if children = Seq.empty then
+            sprintf "<%s/>" openContents
+        else
+            sprintf "<%s>%s</%s>" openContents innerText tagName
---
+++ TaggenScratchPad/Program.fs
@@ -0,0 +1,42 @@
+﻿open Taggen
+
+let f1 = Text("boo")
+let f2 = Frag("p", Seq.empty)
+let f3 = Frag("b", [Text("boo")])
+let f4 = Frag("div.fadeIn.fadeOut#myContent", [f2;f3])
+let f5 x = Frag("div", 
+                [
+                    f4
+                    f2
+                    Frag("p", [x])
+                ]
+             )
+
+let pageFramework pageTitle navItems content =
+    Frag("html",
+            [
+                Frag("Header",
+                        [Frag("Title", [Text(pageTitle)])]
+                    )
+                Frag("Body",
+                        [
+                            Frag("nav", navItems)
+                            Frag("content", content)
+                        ]
+                    )
+            ]
+        )
+
+let navItems =
+    [
+        Frag("ul#navList",
+            [
+                Frag("li#item1", [Text("Item 1")])
+                Frag("li#item2", [Text("Item 2")])
+            ]
+        )
+    ]
+
+do
+    printfn "%s" (printFrag (pageFramework "My page" navItems [(f5 (Text "Boo"))]))
+    System.Console.Read () |> ignore
