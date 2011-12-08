836ff48656bc121cb7d1700a14813e1ac2885e35
Michael Newton <michael@mavnn.co.uk>
2011-12-08 13:25:33 +0000

    Fixed tag closing issue #1

--- Taggen/Taggen.Core.fs
+++ Taggen/Taggen.Core.fs
@@ -4,42 +4,42 @@ open System.Collections
 
 let RegMatch pattern input =
     let matches = Regex.Matches(input, pattern, RegexOptions.Compiled)
-    matches |> Seq.cast
-    |> Seq.map (fun (m : Match) ->
+    matches |> Seq.cast |> Seq.toList
+    |> List.map (fun (m : Match) ->
         [for g in m.Groups -> (g.Value)])
 
-let inline foldStrings strings = Seq.fold (fun acc item -> acc + item) "" strings
+let inline foldStrings strings = List.fold (fun acc item -> acc + item) "" strings
 
 let writeTagname tagText = 
     RegMatch (@"\w+") tagText
-    |> Seq.head 
+    |> List.head 
     |> List.head
 
 let rec writeClasses tagText =
-    let classes = RegMatch (@"\.\w+") tagText |> Seq.concat |> Seq.map (fun dottedName -> dottedName.Trim '.')
-    match Seq.length classes with
+    let classes = RegMatch (@"\.\w+") tagText |> List.concat |> List.map (fun dottedName -> dottedName.Trim '.')
+    match List.length classes with
     | 0 -> ""
-    | 1 -> sprintf " class=\"%s\"" (Seq.head classes)
-    | _ -> Seq.fold (fun acc item -> acc + " " + item) (Seq.head classes) (Seq.skip 1 classes) |> sprintf " class=\"%s\""
+    | 1 -> sprintf " class=\"%s\"" (List.head classes)
+    | _ -> List.fold (fun acc item -> acc + " " + item) (List.head classes) (List.tail classes) |> sprintf " class=\"%s\""
 
 let writeId tagText =
     let matches = RegMatch (@"#\w+") tagText
-    if (Seq.length matches > 1) then failwith "An element can only have one id"
+    if (List.length matches > 1) then failwith "An element can only have one id"
     else
-        Seq.map (fun groups -> groups |> List.map (fun (x : string) -> sprintf " id=\"%s\"" (x.Trim('#'))) |> foldStrings) matches |> foldStrings
+        List.map (fun groups -> groups |> List.map (fun (x : string) -> sprintf " id=\"%s\"" (x.Trim('#'))) |> foldStrings) matches |> foldStrings
 
 let writeAttr attr = 
     match attr with
     | Some m -> 
         m 
-        |> Seq.map (fun (key, value) -> sprintf " %s=\"%s\"" key value)
+        |> List.map (fun (key, value) -> sprintf " %s=\"%s\"" key value)
         |> foldStrings
     | None -> ""
 
 type Fragment =
     | Text of string
-    | Frag of string * seq<Fragment>
-    | FragAttr of string * List<string * string> Option * seq<Fragment> 
+    | Frag of string * List<Fragment>
+    | FragAttr of string * List<string * string> Option * List<Fragment> 
 
 let rec printFrag fragment =
     let strFrag frag =
@@ -49,8 +49,8 @@ let rec printFrag fragment =
         let attrs = writeAttr attr
         let id = writeId tag
         let openContents = sprintf "%s%s%s%s" tagName id classes attrs
-        let innerText = Seq.map printFrag children |> foldStrings
-        if children = Seq.empty then
+        let innerText = List.map printFrag children |> foldStrings
+        if children = List.empty then
             sprintf "<%s/>" openContents
         else
             sprintf "<%s>%s</%s>" openContents innerText tagName
