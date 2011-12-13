61492ad0b7e8ad45e1074e6a4b09c3d74030a9d8
michael.newton <michael.newton@coventry.gov.uk>
2011-12-13 16:13:46 +0000

    Add System.Web, encode html text by default, add RawText fragment type and move to more robust handling of class and id attributes.

--- Taggen/Taggen.Core.fs
+++ Taggen/Taggen.Core.fs
@@ -1,6 +1,7 @@
 ﻿module Taggen.Core
 open System.Text.RegularExpressions
 open System.Collections
+open System.Web
 
 let private RegMatch pattern input =
     let matches = Regex.Matches(input, pattern, RegexOptions.Compiled)
@@ -19,15 +20,15 @@ let rec private writeClasses tagText =
     let classes = RegMatch (@"\.\w+") tagText |> List.concat |> List.map (fun dottedName -> dottedName.Trim '.')
     match List.length classes with
     | 0 -> ""
-    | 1 -> sprintf " class=\"%s\"" (List.head classes)
-    | _ -> List.fold (fun acc item -> acc + " " + item) (List.head classes) (List.tail classes) |> sprintf " class=\"%s\""
+    | 1 -> List.head classes
+    | _ -> List.fold (fun acc item -> acc + " " + item) (List.head classes) (List.tail classes)
 
 let private writeId tagText =
     let matches = RegMatch (@"#\w+") tagText
     if (List.length matches > 1) then 
         failwith "An element can only have one id"
     else
-        List.map (fun groups -> groups |> List.map (fun (x : string) -> sprintf " id=\"%s\"" (x.Trim('#'))) |> foldStrings) matches |> foldStrings
+        List.map (fun groups -> groups |> List.map (fun (x : string) -> x.Trim '#') |> foldStrings) matches |> foldStrings
 
 let private writeAttr attr = 
     match attr with
@@ -39,17 +40,46 @@ let private writeAttr attr =
 
 type Fragment =
     | Text of string
-    | Frag of string * List<Fragment>
+    | RawText of string
     | FragAttr of string * List<string * string> Option * List<Fragment> 
+
+let unpackClasses tag =
+    let classes = writeClasses tag
+    let id = writeId tag
+    if classes <> "" then Some([("class", classes)]) else None
+    |> fun x -> if id <> "" then Some(("id", id)::(match x with | None -> [] | Some x -> x)) else None
+
+let rec updateAttr frag maybeNewAttr =
+    match frag with
+    | FragAttr (tagName, attr, children) ->
+        match maybeNewAttr with
+        | None -> frag
+        | Some newAttr ->
+            match newAttr with
+            | [] -> frag
+            | h::t -> 
+                let replaceAttr currentAttr =
+                    if List.exists (fun old -> (fst old) = (fst h)) currentAttr then
+                        List.map (fun old -> if (fst old) = (fst h) then h else old) currentAttr
+                    else
+                        h::currentAttr
+                let newFrag =
+                    match attr with
+                    | None -> FragAttr (tagName, Some newAttr, children)
+                    | Some oldAttrs -> FragAttr (tagName, Some(replaceAttr oldAttrs), children)
+                updateAttr newFrag (Some t)
+    | _ -> failwith "You can't add attributes to a text fragment."
+    
+
+let Frag (tag, fragments) =
+    let tagName = writeTagname tag
+    FragAttr (tagName, unpackClasses tag, fragments)
 
 let rec printFrag fragment =
     let strFrag frag =
-        let tag, attr, children = frag
-        let tagName = writeTagname tag
-        let classes = writeClasses tag
+        let tagName, attr, children = frag
         let attrs = writeAttr attr
-        let id = writeId tag
-        let openContents = sprintf "%s%s%s%s" tagName id classes attrs
+        let openContents = sprintf "%s%s" tagName attrs
         let innerText = List.map printFrag children |> foldStrings
         if children = List.empty then
             sprintf "<%s/>" openContents
@@ -57,8 +87,7 @@ let rec printFrag fragment =
             sprintf "<%s>%s</%s>" openContents innerText tagName
 
     match fragment with
-    | Text t -> t
-    | Frag (tag, children) ->
-        strFrag (tag, None, children)
+    | Text t -> HttpUtility.HtmlEncode t
+    | RawText t -> t
     | FragAttr (tag, attr, children) ->
         strFrag (tag, attr, children)
