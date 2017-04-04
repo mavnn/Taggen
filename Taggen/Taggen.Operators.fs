03247d0343157fb1832608909be075e82644f717
mavnn <michael@mavnn.co.uk>
2017-04-04 16:00:08 +0100

    Remove + operator

---
+++ Taggen/Taggen.Operators.fs
@@ -0,0 +1,53 @@
+﻿module Taggen.Operators
+
+/// Create Fragment without children
+let (!<>) tagName =
+    Frag (tagName, [])
+
+/// Add child to fragment (as last child)
+let (^<>) fragment child =
+    addChild child fragment
+
+/// Add child to fragment (as last child)
+let (+<>) fragment child =
+    addChild child fragment
+
+/// Add child to fragment (as last child)
+let (|<>) fragment child =
+    addChild child fragment
+
+/// Add inner text to fragment
+let (+~) fragment text =
+    addText text fragment
+
+/// Add inner text to fragment
+let (%) fragment text =
+    addText text fragment
+
+/// Add list of children to fragment
+let (^<<) fragment newChildren =
+    addChildList newChildren fragment
+
+/// Add list of children to fragment
+let (+<<) fragment newChildren =
+    addChildList newChildren fragment
+
+/// Create string representation of Fragment
+let (!) fragment =
+    printFrag fragment
+
+/// Create nicely formatted human readable string representation of Fragment
+let (!!) fragment =
+    prettyPrint fragment
+
+/// Add classes or id to existing fragment using ".class#id" syntax
+let (+.) fragment classes =
+    addClassOrId classes fragment 
+
+/// Add classes or id to existing fragment using ".class#id" syntax
+let (%.) fragment classes =
+    addClassOrId classes fragment
+
+/// Add attributes as list of string, string tuples
+let (++) fragment attrs =
+    addAttrs attrs fragment
